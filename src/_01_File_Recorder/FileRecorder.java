26d8bafe559976f38cd526dd9b1f6617579efca0
HyperBlade3 <65984552+HyperBlade3@users.noreply.github.com>
2020-09-22 17:00:07 -0700

    Module 5 in progress

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,23 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String in = scan.nextLine();
+		try {
+			
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/IO.txt", true);
+			
+			fw.write(in);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
